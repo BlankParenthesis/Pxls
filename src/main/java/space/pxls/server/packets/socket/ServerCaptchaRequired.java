1d0bf071f8e939ff8ec7c27de2a416e086975f9c
[  ] <blank.parenthesis@gmail.com>
2020-09-23 13:29:10 +0100

    Remove unnecessary getter

--- src/main/java/space/pxls/server/packets/socket/ServerCaptchaRequired.java
+++ src/main/java/space/pxls/server/packets/socket/ServerCaptchaRequired.java
@@ -2,8 +2,4 @@ package space.pxls.server.packets.socket;
 
 public class ServerCaptchaRequired {
     public final String type = "captcha_required";
-    
-    public String getType() {
-        return type;
-    }
 }
